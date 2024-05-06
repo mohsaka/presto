a5e0b85be318304aee5eeb88e0859e22c8c2b25f
mohsaka <135669458+mohsaka@users.noreply.github.com>
2024-05-06 10:19:06 -0700

    Update condition for shrink

--- presto-native-execution/presto_cpp/main/LinuxMemoryChecker.cpp
+++ presto-native-execution/presto_cpp/main/LinuxMemoryChecker.cpp
@@ -1,16 +1,39 @@
 #include "presto_cpp/main/LinuxMemoryChecker.h"
 #ifdef __linux__
-#include <sys/sysinfo.h>
-#include <sys/types.h>
+#include <boost/regex.hpp>
+#include <folly/gen/Base.h>
+#include <folly/gen/File.h>
+#include <folly/gen/String.h>
+
+#include <folly/Conv.h>
+#include <folly/CppAttributes.h>
+#include <folly/Format.h>
+#include <folly/Range.h>
+#include <folly/String.h>
 #endif
 
 namespace facebook::presto {
 int64_t LinuxMemoryChecker::systemUsedMemoryBytes() {
 #ifdef __linux__
-  struct sysinfo memInfo;
-  int64_t inUseMemory = 0;
-  sysinfo(&memInfo);
-  return (memInfo.totalram - memInfo.freeram) * memInfo.mem_unit;
+  static const boost::regex memAvailableRegex(R"!(MemAvailable:\s*(\d+)\s*kB)!");
+  static const boost::regex memTotalRegex(R"!(MemTotal:\s*(\d+)\s*kB)!");
+  size_t memAvailable = 0;
+  size_t memTotal = 0;
+  boost::cmatch match;
+
+  folly::gen::byLine("/proc/meminfo") | [&](folly::StringPiece line) -> void {
+    if (boost::regex_match(line.begin(), line.end(), match, memAvailableRegex)) {
+      folly::StringPiece numStr(
+          line.begin() + match.position(1), size_t(match.length(1)));
+      memAvailable = folly::to<size_t>(numStr) * 1024;
+    }
+    if (boost::regex_match(line.begin(), line.end(), match, memTotalRegex)) {
+      folly::StringPiece numStr(
+          line.begin() + match.position(1), size_t(match.length(1)));
+      memTotal = folly::to<size_t>(numStr) * 1024;
+    }
+  };
+  return (memAvailable && memTotal) ? memTotal - memAvailable : 0;
 #else
   return 0;
 #endif
