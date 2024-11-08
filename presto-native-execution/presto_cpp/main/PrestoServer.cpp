83e0ebd37d7d1d4c8674dd35991c963411bdda95
Minhan Cao <mcao@ibm.com>
2024-11-08 13:25:20 -0800

    Stack dump implementation based on Db2
    Alchemy-item: (ID = 583) Stack dump implementation based on db2 commit 1/1 - 74cf7d3d705658a734190b1b0db8097db96e3bb2

--- presto-native-execution/presto_cpp/main/PrestoServer.cpp
+++ presto-native-execution/presto_cpp/main/PrestoServer.cpp
@@ -28,6 +28,7 @@
 #include "presto_cpp/main/common/Utils.h"
 #include "presto_cpp/main/connectors/Registration.h"
 #include "presto_cpp/main/connectors/SystemConnector.h"
+#include "presto_cpp/main/common/process/IBMSignalHandler.h"
 #include "presto_cpp/main/governance/wkc_functions/QaagFunctionsRegistration.h"
 #include "presto_cpp/main/http/HttpConstants.h"
 #include "presto_cpp/main/http/filters/AccessLogFilter.h"
@@ -488,6 +489,12 @@ void PrestoServer::run() {
   prestoServerOperations_ =
       std::make_unique<PrestoServerOperations>(taskManager_.get(), this);
   registerSystemConnector();
+
+  auto ibmSignalHandler =
+      folly::Singleton<facebook::presto::process::IBMSignalHandler>::try_get();
+  if (ibmSignalHandler) {
+    ibmSignalHandler->setTaskManager(taskManager_.get());
+  }
 
   // The endpoint used by operation in production.
   httpServer_->registerGet(
