94ee98612021557ec07a17e6822424fee8f5c884
Anant Aneja <anant.aneja@ibm.com>
2025-06-09 16:39:17 +0530

    [Opt Plus Only Feature] Allow users to disable join flipping
    There are cases where users want to disable cost-based join
    re-ordering, but still want the join type to be automatically
    determined. Before this change, it was possible for the
    DetermineJoinDistributionType rule to flip the join order
    using statistics when it was undesirable.
    
    This adds a new feature config for the coordinator and a
    corresponding session property.
    
    - feature config: optimizer.size-based-join-flipping-enabled
    - session property: optimizer_size_based_join_flipping_enabled
    
    By default, this is true to preserve the previous behavior, but setting
    it to false will disable returning any results which flip the sides of
    the join in the DetermineJoinDistributionType rule.
    
    Cherry-pick of bcaa3f1
    
    Alchemy-item: (ID = 266) [Opt Plus Only Feature] Allow users to disable join flipping commit 1/1 - 4429263957dafa084b4b9f968a2dd9feaf5e385a

--- presto-main-base/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
+++ presto-main-base/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
@@ -59,6 +59,7 @@ public class TestFeaturesConfig
     public void testDefaults()
     {
         assertRecordedDefaults(ConfigAssertions.recordDefaults(FeaturesConfig.class)
+                .setSizeBasedJoinFlippingEnabled(true)
                 .setCpuCostWeight(75)
                 .setMemoryCostWeight(10)
                 .setNetworkCostWeight(15)
@@ -275,6 +276,7 @@ public class TestFeaturesConfig
     public void testExplicitPropertyMappings()
     {
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("optimizer.size-based-join-flipping-enabled", "false")
                 .put("cpu-cost-weight", "0.4")
                 .put("memory-cost-weight", "0.3")
                 .put("network-cost-weight", "0.2")
@@ -486,6 +488,7 @@ public class TestFeaturesConfig
                 .build();
 
         FeaturesConfig expected = new FeaturesConfig()
+                .setSizeBasedJoinFlippingEnabled(false)
                 .setCpuCostWeight(0.4)
                 .setMemoryCostWeight(0.3)
                 .setNetworkCostWeight(0.2)
