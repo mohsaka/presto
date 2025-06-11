aa7840e8ef768c9985450c4abab9a5df13fbf0fc
Prashant Sharma <prashsh1@in.ibm.com>
2025-06-11 17:23:29 +0530

    Forward fit Max prefixes count PR #411.
    make MAX_PREFIXES_COUNT configurable via session properties and config.properties.
    This helps improve performance of SHOW QUERY.
    
    Alchemy-item: (ID = 234) [Forward-fit] max prefixes count commit 1/1 - 33721dc6b7280f7956aaf4e5060822a3158d0726

--- presto-main-base/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
+++ presto-main-base/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
@@ -60,6 +60,7 @@ public class TestFeaturesConfig
     {
         assertRecordedDefaults(ConfigAssertions.recordDefaults(FeaturesConfig.class)
                 .setSizeBasedJoinFlippingEnabled(true)
+                .setMaxPrefixesCount(100)
                 .setCpuCostWeight(75)
                 .setMemoryCostWeight(10)
                 .setNetworkCostWeight(15)
@@ -279,6 +280,7 @@ public class TestFeaturesConfig
     public void testExplicitPropertyMappings()
     {
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("max-prefixes-count", "1")
                 .put("optimizer.size-based-join-flipping-enabled", "false")
                 .put("cpu-cost-weight", "0.4")
                 .put("memory-cost-weight", "0.3")
@@ -494,6 +496,7 @@ public class TestFeaturesConfig
                 .build();
 
         FeaturesConfig expected = new FeaturesConfig()
+                .setMaxPrefixesCount(1)
                 .setSizeBasedJoinFlippingEnabled(false)
                 .setCpuCostWeight(0.4)
                 .setMemoryCostWeight(0.3)
