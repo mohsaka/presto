ab71261d502b8b0ebc055914b2b3f73248447a59
Anant Aneja <1797669+aaneja@users.noreply.github.com>
2025-06-18 15:38:10 +0530

    Changes to support Join Pushdown for JDBC Connectors
    Co-Authored-By: Haritha Koloth <haritha.k@ibm.com>
    Co-Authored-By: Glerin Pinhero <glerin.pinhero@ibm.com>
    Co-Authored-By: Thanzeel Hassan <thanzeel.hassan@ibm.com>
    Co-Authored-By: Anant Aneja <anant.aneja@ibm.com>
    
    Alchemy-item: (ID = 386) Changes to support Join Pushdown for JDBC Connectors commit 1/3 - 2b9699b0

--- presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlClient.java
+++ presto-mysql/src/main/java/com/facebook/presto/plugin/mysql/MySqlClient.java
@@ -237,10 +237,12 @@ public class MySqlClient
                 split.getCatalogName(),
                 split.getSchemaName(),
                 split.getTableName(),
+                split.getJoinTables(),
                 columnHandles,
                 columnExpressions,
                 split.getTupleDomain(),
-                split.getAdditionalPredicate());
+                split.getAdditionalPredicate(),
+                split.getTableAlias());
     }
 
     @Override
