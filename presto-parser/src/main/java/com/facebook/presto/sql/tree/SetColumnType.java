75b7fb4dd669ec1b93608e77b13f95a9c0bd6fc4
Nidhin Varghese <Nidhin.Varghese1@ibm.com>
2025-07-16 14:27:18 +0530

    Add implementation of UpdateInfo
    Alchemy-item: (ID = 365) Add support for ALTER COLUMN SET DATA TYPE in the Iceberg connector commit 2/2 - 15d186411696a4b63297732eb4c44a5c1017b8a4

--- presto-parser/src/main/java/com/facebook/presto/sql/tree/SetColumnType.java
+++ presto-parser/src/main/java/com/facebook/presto/sql/tree/SetColumnType.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.sql.tree;
 
+import com.facebook.presto.spi.analyzer.UpdateInfo;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
@@ -69,6 +70,12 @@ public class SetColumnType
     public List<TransactionMode> getChildren()
     {
         return ImmutableList.of();
+    }
+
+    @Override
+    public UpdateInfo getUpdateInfo()
+    {
+        return new UpdateInfo("SET COLUMN TYPE", tableName.toString());
     }
 
     @Override
