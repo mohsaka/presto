48b598b71d67858e141b6b30e8d19ecc266dc9c1
mohsaka <135669458+mohsaka@users.noreply.github.com>
2026-04-23 10:49:46 -0700

    Add zorder functionality with row type

--- presto-main-base/src/main/java/com/facebook/presto/sql/planner/LogicalPlanner.java
+++ presto-main-base/src/main/java/com/facebook/presto/sql/planner/LogicalPlanner.java
@@ -17,8 +17,8 @@ import com.facebook.presto.Session;
 import com.facebook.presto.common.QualifiedObjectName;
 import com.facebook.presto.common.block.BlockBuilder;
 import com.facebook.presto.common.predicate.TupleDomain;
+import com.facebook.presto.common.type.RowType;
 import com.facebook.presto.common.type.Type;
-import com.facebook.presto.metadata.CastType;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.metadata.TableLayout;
 import com.facebook.presto.metadata.TableLayout.TablePartitioning;
@@ -55,6 +55,7 @@ import com.facebook.presto.spi.plan.ValuesNode;
 import com.facebook.presto.spi.procedure.DistributedProcedure;
 import com.facebook.presto.spi.relation.CallExpression;
 import com.facebook.presto.spi.relation.RowExpression;
+import com.facebook.presto.spi.relation.SpecialFormExpression;
 import com.facebook.presto.spi.relation.VariableReferenceExpression;
 import com.facebook.presto.spi.statistics.TableStatisticsMetadata;
 import com.facebook.presto.sql.ExpressionFormatter;
@@ -127,6 +128,7 @@ import static com.facebook.presto.spi.plan.TableWriterNode.CreateName;
 import static com.facebook.presto.spi.plan.TableWriterNode.InsertReference;
 import static com.facebook.presto.spi.plan.TableWriterNode.RefreshMaterializedViewReference;
 import static com.facebook.presto.spi.plan.TableWriterNode.UpdateTarget;
+import static com.facebook.presto.spi.relation.SpecialFormExpression.Form.ROW_CONSTRUCTOR;
 import static com.facebook.presto.spi.statistics.TableStatisticType.ROW_COUNT;
 import static com.facebook.presto.sql.TemporaryTableUtil.splitIntoPartialAndFinal;
 import static com.facebook.presto.sql.analyzer.ExpressionTreeUtils.createSymbolReference;
@@ -324,14 +326,16 @@ public class LogicalPlanner
                 .map(TableDataRewriteAnalysisContext.class::cast)
                 .flatMap(TableDataRewriteAnalysisContext::getzOrderColumns);
         if (zOrderColumns.isPresent() && !zOrderColumns.get().isEmpty()) {
-            // todo: use the column names in `zOrderColumns` to construct a `zorder` function to replace to `to_utf8` function here.
-            VariableReferenceExpression orderCol1 = columnToVariableMap.get(zOrderColumns.get().get(0));
-            CallExpression castToVarchar = call("CAST", metadata.getFunctionAndTypeManager().lookupCast(CastType.CAST, orderCol1.getType(), VARCHAR), VARCHAR, orderCol1);
-            CallExpression varbinaryFunction = call("to_utf8", metadata.getFunctionAndTypeManager().lookupFunction("to_utf8", fromTypes(VARCHAR)), VARBINARY, castToVarchar);
+            List<VariableReferenceExpression> zOrderColumnVars = zOrderColumns.get().stream()
+                    .map(columnToVariableMap::get)
+                    .collect(toImmutableList());
+
+            CallExpression zorderFunction = buildZOrderFunctionCall(zOrderColumnVars, metadata);
+
             VariableReferenceExpression output = variableAllocator.newVariable("$z_order", VARBINARY);
             Assignments assignments = Assignments.builder()
                     .putAll(identityAssignments(queryRoot.getOutputVariables()))
-                    .put(output, varbinaryFunction)
+                    .put(output, zorderFunction)
                     .build();
             queryRoot = new ProjectNode(idAllocator.getNextId(), queryRoot, assignments);
             columnNames = ImmutableList.<String>builder().addAll(columnNames).add("$z_order").build();
@@ -1029,5 +1033,35 @@ public class LogicalPlanner
                     tableLayout.get().getWriterPolicy() == MULTIPLE_WRITERS_PER_PARTITION_ALLOWED));
         }
         return partitioningScheme;
+    }
+
+    /**
+     * Builds a zorder function call expression: zorder(ROW(col1, col2, ...))
+     *
+     * @param columns the columns to include in the z-order calculation
+     * @param metadata the metadata for function lookup
+     * @return a CallExpression representing the zorder function call
+     */
+    private static CallExpression buildZOrderFunctionCall(List<VariableReferenceExpression> columns, Metadata metadata)
+    {
+        // Create anonymous ROW type from column types
+        List<Type> columnTypes = columns.stream()
+                .map(VariableReferenceExpression::getType)
+                .collect(toImmutableList());
+        RowType rowType = RowType.anonymous(columnTypes);
+
+        // Create ROW constructor: ROW(col1, col2, ...)
+        List<RowExpression> rowArguments = columns.stream()
+                .map(var -> (RowExpression) var)
+                .collect(toImmutableList());
+        SpecialFormExpression rowConstructor = new SpecialFormExpression(
+                ROW_CONSTRUCTOR,
+                rowType,
+                rowArguments);
+
+        // Create and return zorder function call: zorder(ROW(col1, col2, ...))
+        FunctionHandle zorderFunctionHandle = metadata.getFunctionAndTypeManager()
+                .lookupFunction("zorder", fromTypes(rowType));
+        return call("zorder", zorderFunctionHandle, VARBINARY, rowConstructor);
     }
 }
